5ddce52d94719de0ba70b755c9e96df3efc9bf14
Parker Berberian <pberberian@iol.unh.edu>
2019-06-27 16:59:48 -0400

    Removed Redundant Variables
    For a deployment to work, e.g. DB_USER and POSTGRES_USER
    must be the same. Docker compose does not allow for variable
    expansion so I delete the redundant variables.
    
    Change-Id: I43b83f4d5fceaecc8bcf773935d5b5da084976e6
    Signed-off-by: Parker Berberian <pberberian@iol.unh.edu>

--- src/pharos_dashboard/settings.py
+++ src/pharos_dashboard/settings.py
@@ -135,9 +135,9 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASS'],
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': os.environ['DB_SERVICE'],
         'PORT': os.environ['DB_PORT']
     }
