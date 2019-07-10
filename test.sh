e30226b015aac5062f3bccc30c96884de5f85b6a
Sawyer Bergeron <sbergeron@iol.unh.edu>
2019-07-10 15:33:49 -0400

    Generate coverage report for tests
    Change-Id: I11ffe8c208f9f1e7d06d340e6cf6ac6116839836
    Signed-off-by: Sawyer Bergeron <sbergeron@iol.unh.edu>

--- test.sh
+++ test.sh
@@ -13,4 +13,4 @@ find . -type f -name "*.py" -not -name "manage.py" | xargs flake8 --count --igno
 
 
 # this file should be executed from the dir it is in
-docker exec -it dg01 python manage.py test
+docker exec -it dg01 coverage run --source='.' manage.py test
