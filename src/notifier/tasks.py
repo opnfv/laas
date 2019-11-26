2b8e19c5bf31e40a2c177df1cfcceb3c1acd5917
Brandon Lo <lobrandon1217@gmail.com>
2019-11-26 16:39:26 -0500

    Add warning email and notification
    This adds the abandoned changes made to the notification
    system and also adds a simple task to check for expiring
    bookings and sends out emails and notifications.
    
    Change-Id: I1530d19f41cf93626bb642e6b269f9ec55860b81
    Signed-off-by: Brandon Lo <lobrandon1217@gmail.com>

---
+++ src/notifier/tasks.py
@@ -0,0 +1,34 @@
+##############################################################################
+# Copyright (c) 2016 Max Breitenfeldt and others.
+# Copyright (c) 2018 Parker Berberian, Sawyer Bergeron, and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+from celery import shared_task
+from django.utils import timezone
+from booking.models import Booking
+from notifier.models import Emailed
+from notifier.manager import NotificationHandler
+
+
+@shared_task
+def notify_expiring():
+    """
+    Notify users if their booking is within 48 hours of expiring.
+    """
+    # Expiring within 48 hours
+    expire_time = timezone.now() + timezone.timedelta(hours=48)
+    # Don't email people about bookings that have started recently
+    start_time = timezone.now() - timezone.timedelta(hours=12)
+    bookings = Booking.objects.filter(end__lte=expire_time,
+        start__lte=start_time)
+    for booking in bookings:
+        if Emailed.objects.filter(almost_end_booking=booking).exists():
+            continue
+        NotificationHandler.notify_booking_expiring(booking)
+        Emailed.objects.create(almost_end_booking=booking)
