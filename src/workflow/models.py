d7c0a4b432903c831c04731cc80a003532281e82
Parker Berberian <pberberian@iol.unh.edu>
2019-12-19 12:39:01 -0500

    Comments and Documentation
    This change adds a ton of comments and documentation across all the code.
    I am not adding to the api or resource apps yet because they are undergoing changes
    
    Change-Id: Ifee0a2f534e8584f14b0f13af4dda8dc70eb7553
    Signed-off-by: Parker Berberian <pberberian@iol.unh.edu>

--- src/workflow/models.py
+++ src/workflow/models.py
@@ -26,6 +26,14 @@ from booking.models import Booking
 
 
 class BookingAuthManager():
+    """
+    Verifies Booking Authorization
+
+    Class to verify that the user is allowed to book the requested resource
+    The user must input a url to the INFO.yaml file to prove that they are the ptl of
+    an approved project if they are booking a multi-node pod.
+    This class parses the url and checks the logged in user against the info file.
+    """
     LFN_PROJECTS = ["opnfv"]  # TODO
 
     def parse_github_url(self, url):
@@ -158,6 +166,13 @@ class BookingAuthManager():
 
 
 class WorkflowStepStatus(object):
+    """
+    Poor man's enum for the status of a workflow step
+
+    The steps in a workflow are not completed (UNTOUCHED)
+    or they have been completed correctly (VALID) or they were filled out
+    incorrectly (INVALID)
+    """
     UNTOUCHED = 0
     INVALID = 100
     VALID = 200
