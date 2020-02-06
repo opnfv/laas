d410bb236fd02c7dac3df99b3532d88a6b174564
Parker Berberian <pberberian@iol.unh.edu>
2020-02-06 12:59:51 -0500

    Modifies Resource Models for ongoing refactor
    Change-Id: Ice88f53135f57aca8e2de4d69274e7d490f981a4
    Signed-off-by: Parker Berberian <pberberian@iol.unh.edu>

--- src/resource_inventory/models.py
+++ src/resource_inventory/models.py
@@ -17,9 +17,8 @@ from account.models import Lab
 
 
 # profile of resources hosted by labs
-class HostProfile(models.Model):
+class ResourceProfile(models.Model):
     id = models.AutoField(primary_key=True)
-    host_type = models.PositiveSmallIntegerField(default=0)
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
     labs = models.ManyToManyField(Lab, related_name="hostprofiles")
@@ -39,7 +38,7 @@ class InterfaceProfile(models.Model):
     id = models.AutoField(primary_key=True)
     speed = models.IntegerField()
     name = models.CharField(max_length=100)
-    host = models.ForeignKey(HostProfile, on_delete=models.CASCADE, related_name='interfaceprofile')
+    host = models.ForeignKey(ResourceProfile, on_delete=models.CASCADE, related_name='interfaceprofile')
     nic_type = models.CharField(
         max_length=50,
         choices=[
@@ -48,6 +47,7 @@ class InterfaceProfile(models.Model):
         ],
         default="onboard"
     )
+    order = models.IntegerField(default=-1)
 
     def __str__(self):
         return self.name + " for " + str(self.host)
@@ -61,7 +61,7 @@ class DiskProfile(models.Model):
         ("HDD", "HDD")
     ])
     name = models.CharField(max_length=50)
-    host = models.ForeignKey(HostProfile, on_delete=models.CASCADE, related_name='storageprofile')
+    host = models.ForeignKey(ResourceProfile, on_delete=models.CASCADE, related_name='storageprofile')
     rotation = models.IntegerField(default=0)
     interface = models.CharField(
         max_length=50,
@@ -88,7 +88,7 @@ class CpuProfile(models.Model):
         ("aarch64", "aarch64")
     ])
     cpus = models.IntegerField()
-    host = models.ForeignKey(HostProfile, on_delete=models.CASCADE, related_name='cpuprofile')
+    host = models.ForeignKey(ResourceProfile, on_delete=models.CASCADE, related_name='cpuprofile')
     cflags = models.TextField(null=True)
 
     def __str__(self):
@@ -99,15 +99,24 @@ class RamProfile(models.Model):
     id = models.AutoField(primary_key=True)
     amount = models.IntegerField()
     channels = models.IntegerField()
-    host = models.ForeignKey(HostProfile, on_delete=models.CASCADE, related_name='ramprofile')
+    host = models.ForeignKey(ResourceProfile, on_delete=models.CASCADE, related_name='ramprofile')
 
     def __str__(self):
         return str(self.amount) + "G for " + str(self.host)
 
 
 class Resource(models.Model):
+
     class Meta:
         abstract = True
+
+    bundle = models.ForeignKey(ResourceBundle, on_delete=models.set_null, null=True)
+    profile = models.ForeignKey(ResourceProfile, on_delete=models.CASCADE)
+    config = models.ForeignKey(ResourceConfig, on_delete=models.set_null, null=True)
+    working = models.BooleanField(default=True)
+    vendor = models.CharField(max_length=100, default="unknown")
+    model = models.CharField(max_length=150, default="unknown")
+    interfaces = models.ManyToManyField(Interface)
 
     def get_configuration(self, state):
         """
@@ -133,7 +142,8 @@ class Resource(models.Model):
 
 
 # Generic resource templates
-class GenericResourceBundle(models.Model):
+class ResourceTemplate(models.Model):
+    # TODO: template might not be a good name because this is a collection of lots of configured resources
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=300, unique=True)
     xml = models.TextField()
@@ -144,6 +154,7 @@ class GenericResourceBundle(models.Model):
     hidden = models.BooleanField(default=False)
 
     def getResources(self):
+        # TODO
         my_resources = []
         for genericResource in self.generic_resources.all():
             my_resources.append(genericResource.getResource())
@@ -157,7 +168,7 @@ class GenericResourceBundle(models.Model):
 class Network(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    bundle = models.ForeignKey(GenericResourceBundle, on_delete=models.CASCADE, related_name="networks")
+    bundle = models.ForeignKey(ResourceTemplate, on_delete=models.CASCADE, related_name="networks")
     is_public = models.BooleanField()
 
     def __str__(self):
@@ -209,53 +220,30 @@ class ConfigState:
     CLEAN = 200
 
 
-class GenericResource(models.Model):
-    bundle = models.ForeignKey(GenericResourceBundle, related_name='generic_resources', on_delete=models.CASCADE)
-    hostname_validchars = RegexValidator(regex=r'(?=^.{1,253}$)(?=(^([A-Za-z0-9\-\_]{1,62}\.)*[A-Za-z0-9\-\_]{1,63}$))', message="Enter a valid hostname. Full domain name may be 1-253 characters, each hostname 1-63 characters (including suffixed dot), and valid characters for hostnames are A-Z, a-z, 0-9, hyphen (-), and underscore (_)")
-    name = models.CharField(max_length=200, validators=[hostname_validchars])
-
-    def getResource(self):
-        # TODO: This will have to be dealt with
-        return self.generic_host
-
-    def __str__(self):
-        return self.name
-
-    def validate(self):
-        validname = re.compile(r'(?=^.{1,253}$)(?=(^([A-Za-z0-9\-\_]{1,62}\.)*[A-Za-z0-9\-\_]{1,63}$))')
-        if not validname.match(self.name):
-            return "Enter a valid hostname. Full domain name may be 1-253 characters, each hostname 1-63 characters (including suffixed dot), and valid characters for hostnames are A-Z, a-z, 0-9, hyphen (-), and underscore (_)"
-        else:
-            return None
-
-
-# Host template
-class GenericHost(models.Model):
-    id = models.AutoField(primary_key=True)
-    profile = models.ForeignKey(HostProfile, on_delete=models.CASCADE)
-    resource = models.OneToOneField(GenericResource, related_name='generic_host', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.resource.name
-
-
 # Physical, actual resources
 class ResourceBundle(Resource):
-    template = models.ForeignKey(GenericResourceBundle, on_delete=models.SET_NULL, null=True)
+    template = models.ForeignKey(ResourceTemplate, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         if self.template is None:
             return "Resource bundle " + str(self.id) + " with no template"
         return "instance of " + str(self.template)
 
-    def get_host(self, role="Jumphost"):
-        return Host.objects.filter(bundle=self, config__is_head_node=True).first()  # should only ever be one, but it is not an invariant in the models
+    def get_resources(self):
+        my_resources = []
+        for cls in ResourceManager.RESOURCE_TYPES:
+            my_resources += list(cls.objects.filter(bundle=self))
+        return my_resources
+
+    def get_resource_with_role(self, role):
+        # TODO
+        pass
 
 
-class GenericInterface(models.Model):
+class InterfaceConfiguration(models.Model):
     id = models.AutoField(primary_key=True)
     profile = models.ForeignKey(InterfaceProfile, on_delete=models.CASCADE)
-    host = models.ForeignKey(GenericHost, on_delete=models.CASCADE, related_name='generic_interfaces')
+    resource_config = models.ForeignKey(ResourceConfiguration, on_delete=models.CASCADE, related_name='interface_configs')
     connections = models.ManyToManyField(NetworkConnection)
 
     def __str__(self):
@@ -293,19 +281,6 @@ class NetworkRole(models.Model):
     network = models.ForeignKey(Network, on_delete=models.CASCADE)
 
 
-class ConfigBundle(models.Model):
-    id = models.AutoField(primary_key=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, unique=True)
-    description = models.CharField(max_length=1000, default="")
-    bundle = models.ForeignKey(GenericResourceBundle, null=True, on_delete=models.CASCADE)
-    public = models.BooleanField(default=False)
-    hidden = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
-
-
 class OPNFVConfig(models.Model):
     id = models.AutoField(primary_key=True)
     installer = models.ForeignKey(Installer, on_delete=models.CASCADE)
@@ -338,7 +313,7 @@ class Image(models.Model):
     name = models.CharField(max_length=200)
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     public = models.BooleanField(default=True)
-    host_type = models.ForeignKey(HostProfile, on_delete=models.CASCADE)
+    host_type = models.ForeignKey(ResourceProfile, on_delete=models.CASCADE)
     description = models.TextField()
     os = models.ForeignKey(Opsys, null=True, on_delete=models.CASCADE)
 
@@ -353,15 +328,15 @@ def get_sentinal_opnfv_role():
     return OPNFVRole.objects.get_or_create(name="deleted", description="Role was deleted.")
 
 
-class HostConfiguration(models.Model):
+class ResourceConfiguration(models.Model):
     """
     model to represent a complete configuration for a single
     physical host
     """
     id = models.AutoField(primary_key=True)
-    host = models.ForeignKey(GenericHost, related_name="configuration", on_delete=models.CASCADE)
+    profile = models.ForeignKey(ResourceProfile, on_delete=models.CASCADE)
     image = models.ForeignKey(Image, on_delete=models.PROTECT)
-    bundle = models.ForeignKey(ConfigBundle, related_name="hostConfigurations", null=True, on_delete=models.CASCADE)
+    template = models.ForeignKey(ResourceTemplate, related_name="resourceConfigurations", null=True, on_delete=models.CASCADE)
     is_head_node = models.BooleanField(default=False)
 
     def __str__(self):
@@ -395,18 +370,11 @@ def get_default_remote_info():
 
 
 # Concrete host, actual machine in a lab
-class Host(Resource):
-    template = models.ForeignKey(GenericHost, on_delete=models.SET_NULL, null=True)
+class Server(Resource):
     booked = models.BooleanField(default=False)
     name = models.CharField(max_length=200, unique=True)
-    bundle = models.ForeignKey(ResourceBundle, related_name='hosts', on_delete=models.SET_NULL, null=True)
-    config = models.ForeignKey(HostConfiguration, null=True, related_name="configuration", on_delete=models.SET_NULL)
     labid = models.CharField(max_length=200, default="default_id")
-    profile = models.ForeignKey(HostProfile, on_delete=models.CASCADE)
     lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
-    working = models.BooleanField(default=True)
-    vendor = models.CharField(max_length=100, default="unknown")
-    model = models.CharField(max_length=150, default="unknown")
     remote_management = models.ForeignKey(RemoteInfo, default=get_default_remote_info, on_delete=models.SET(get_default_remote_info))
 
     def __str__(self):
@@ -431,7 +399,6 @@ class Interface(models.Model):
     bus_address = models.CharField(max_length=50)
     name = models.CharField(max_length=100, default="eth0")
     config = models.ManyToManyField(Vlan)
-    host = models.ForeignKey(Host, on_delete=models.CASCADE, related_name='interfaces')
 
     def __str__(self):
         return self.mac_address + " on host " + str(self.host)
@@ -443,3 +410,9 @@ class OPNFV_SETTINGS():
     """
     # all the required network types in PDF/IDF spec
     NETWORK_ROLES = ["public", "private", "admin", "mgmt"]
+
+
+class ResourceManager():
+    """ Manage Resources."""
+
+    RESOURCE_TYPES = [Server]
