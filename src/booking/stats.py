a2c82d53bd7a3a9bb786f98d37534984a8c844ba
Sean Smith <ssmith@iol.unh.edu>
2020-07-07 12:08:41 -0400

    Analytics Board
    Signed-off-by: Sean Smith <ssmith@iol.unh.edu>
    Change-Id: Ie98daf209d48220797e63304a80778d19711b676

--- src/booking/stats.py
+++ src/booking/stats.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2020 Parker Berberian, Sawyer Bergeron, Sean Smith and others.
+# Copyright (c) 2020 Sean Smith and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
@@ -10,13 +10,23 @@ from booking.models import Booking
 from datetime import datetime, timedelta
 import pytz
 
+from resource_inventory.models import (
+    CpuProfile,
+    ResourceQuery
+)
+
+from collections import Counter
+
 
 class StatisticsManager(object):
 
     @staticmethod
     def getContinuousBookingTimeSeries(span=28):
         """
-        Calculate Booking usage data points.
+        This function drives the analytics board.
+
+        First this calculates statistics that are static in the time period.
+        Second it calcalulates active bookings and active users.
 
         Gathers all active bookings that fall in interval [(now - span), (now + 1 week)].
         x data points are every 12 hours
@@ -28,24 +38,30 @@ class StatisticsManager(object):
         users = []
 
         now = datetime.now(pytz.utc)
-        delta = timedelta(days=span)
+        delta = timedelta(days=365)
         start = now - delta
         end = now + timedelta(weeks=1)
 
         bookings = Booking.objects.filter(
             start__lte=end,
             end__gte=start
-        ).prefetch_related("collaborators")
+        )
 
-        # get data
+        # bundles = [book.resource for book in bookings]
+        # resources = [ResourceQuery.filter(bundle=bundle) for bundle in bundles]
+
+        # arch_dict = StatisticsManager.hosts_booked(bookings, resources)
+        # hosts = StatisticsManager.hosts_per_booking(bookings, resources)
+
         while start <= end:
             active_users = 0
 
-            books = bookings.filter(
+            books = Booking.objects.filter(
                 start__lte=start,
                 end__gte=start
             ).prefetch_related("collaborators")
 
+            # This will be aged out for FreeIPA active users
             for booking in books:
                 active_users += booking.collaborators.all().count() + 1
 
@@ -56,3 +72,34 @@ class StatisticsManager(object):
             start += timedelta(hours=12)
 
         return {"booking": [x, y], "user": [x, users]}
+
+    @staticmethod
+    def hosts_booked(bookings, resources):
+        """ Find how many of each architecture is booked """
+        flat_resources = [resource for booking in resources for resource in booking]
+
+        active_x86 = 0
+        for resource in flat_resources:
+            active_x86 += CpuProfile.objects.filter(
+                architecture='x86_64',
+                host=resource.profile
+            ).count()
+
+        active_arm = len(flat_resources) - active_x86
+
+        return {'x86 Servers': active_x86, 'arm Servers': active_arm}
+
+    @staticmethod
+    def hosts_per_booking(bookings, resources):
+        """ Get number of hosts in a booking for all bookings"""
+        return [len(book) for book in resources]
+
+    @staticmethod
+    def collaborators_per_booking(bookings):
+        """ Get number of collaborators for bookings """
+        return [book.collaborators.all().count() for book in bookings]
+
+    @staticmethod
+    def user_count_bookings(bookings):
+        """ Find how many bookings each user has open """
+        return dict(Counter([book.owner for book in bookings]))
