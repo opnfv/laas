d249495b662926e741db488db30992e994d06eb7
Sawyer Bergeron <sbergeron@iol.unh.edu>
2020-10-22 13:06:39 -0400

    Fix quick booking allocation of private vlans for pods
    Signed-off-by: Sawyer Bergeron <sbergeron@iol.unh.edu>
    Change-Id: Ie515da2c28fcbc51d8fa87e24a3ff64c234d1bee
    Signed-off-by: Sawyer Bergeron <sbergeron@iol.unh.edu>

--- src/account/models.py
+++ src/account/models.py
@@ -80,9 +80,9 @@ class VlanManager(models.Model):
     # if they use QinQ or a vxlan overlay, for example
     allow_overlapping = models.BooleanField()
 
-    def get_vlan(self, count=1):
+    def get_vlans(self, count=1):
         """
-        Return the ID of available vlans, but does not reserve them.
+        Return the IDs of available vlans as a list[int], but does not reserve them.
 
         Will throw index exception if not enough vlans are available.
         If count == 1, the return value is an int. Otherwise, it is a list of ints.
