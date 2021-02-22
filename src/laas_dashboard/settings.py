67b34aef76cb7add17eb29c41ff5ae27bdc64d40
Sean Smith <ssmith@iol.unh.edu>
2021-02-22 13:05:46 -0500

    Update celery schedule to send emails
    Change-Id: I0fbf7080d440cea98d358a30ac5df88ce888a8a0
    Signed-off-by: Sean Smith <ssmith@iol.unh.edu>

--- src/laas_dashboard/settings.py
+++ src/laas_dashboard/settings.py
@@ -240,7 +240,11 @@ CELERYBEAT_SCHEDULE = {
     'query_vpn_users': {
         'task': 'dashboard.tasks.query_vpn_users',
         'schedule': timedelta(hours=1)
-    }
+    },
+    'dispatch_emails': {
+        'task': 'notifier.tasks.dispatch_emails', 
+        'schedule': timedelta(hours=1)
+    },
 }
 
 # Notifier Settings
