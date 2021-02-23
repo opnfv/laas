625b6cf45ff86f16d4b98a661aa26563ca315823
Sean Smith <ssmith@iol.unh.edu>
2021-02-23 15:41:59 -0500

    Update actions to free hosts and vlans
    Signed-off-by: Sean Smith <ssmith@iol.unh.edu>
    Change-Id: I94425ca5a48cccdf0b5382ef2bb16a989ee6b32d

--- src/dashboard/actions.py
+++ src/dashboard/actions.py
@@ -1,26 +1,24 @@
-##############################################################################
+###############################################################################
 # Copyright (c) 2019 Parker Berberian, Sawyer Bergeron, and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
-##############################################################################
+###############################################################################
 
-from resource_inventory.models import Host, Vlan
+from resource_inventory.models import ResourceQuery, Vlan
 from account.models import Lab
 from booking.models import Booking
-from datetime import timedelta
-from django.utils import timezone
+from datetime import timedelta, datetime
+import pytz
 
 
 def free_leaked_hosts(free_old_bookings=False, old_booking_age=timedelta(days=1)):
-    bundles = [booking.resource for booking in Booking.objects.filter(end__gt=timezone.now())]
-    active_hosts = set()
-    for bundle in bundles:
-        active_hosts.update([host for host in bundle.hosts.all()])
 
-    marked_hosts = set(Host.objects.filter(booked=True))
+    bundles = [booking.resource for booking in Booking.objects.filter(end__gt=datetime.now(pytz.utc))]
+    active_hosts = set(ResourceQuery.filter(bundle__in=bundles))
+    marked_hosts = set(ResourceQuery.filter(booked=True, working=True))
 
     for host in (marked_hosts - active_hosts):
         host.booked = False
@@ -28,15 +26,15 @@ def free_leaked_hosts(free_old_bookings=False, old_booking_age=timedelta(days=1)
 
 
 def free_leaked_public_vlans():
-    booked_host_interfaces = []
 
     for lab in Lab.objects.all():
+        booked_interfaces = set([
+            iface
+            for host in ResourceQuery.filter(booked=True, lab=lab)
+            for iface in host.interfaces.all()
+        ])
 
-        for host in Host.objects.filter(booked=True).filter(lab=lab):
-            for interface in host.interfaces.all():
-                booked_host_interfaces.append(interface)
-
-        in_use_vlans = Vlan.objects.filter(public=True).distinct('vlan_id').filter(interface__in=booked_host_interfaces)
+        in_use_vlans = Vlan.objects.filter(public=True).distinct('vlan_id').filter(interface__in=booked_interfaces)
 
         manager = lab.vlan_manager
 
