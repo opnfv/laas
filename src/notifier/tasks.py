70797f397d24c32367072d6b9acda8981d6394eb
Sean Smith <ssmith@iol.unh.edu>
2021-03-25 15:16:50 -0400

    Additional fix for email
    Signed-off-by: Sean Smith <ssmith@iol.unh.edu>
    Change-Id: I0599e29c7930386651f77bceccf05d069ebd4101

--- src/notifier/tasks.py
+++ src/notifier/tasks.py
@@ -47,5 +47,7 @@ def dispatch_emails():
             email.title,
             email.message,
             os.environ.get("DEFAULT_FROM_EMAIL", "opnfv@laas-dashboard"),
-            email.recipient,
+            [email.recipient],
             fail_silently=False)
+            print('aa')
+
