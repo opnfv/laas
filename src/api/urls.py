cdc80c331b2fd000ce0c10698effd4e826c7f3ee
Sawyer Bergeron <sbergeron@iol.unh.edu>
2021-10-29 15:27:32 -0400

    Start adding cloud config support to booking api
    Signed-off-by: Sawyer Bergeron <sbergeron@iol.unh.edu>
    Change-Id: Idbad76db40b08e41cdc41789b487c9d476852c57
    Signed-off-by: Sawyer Bergeron <sbergeron@iol.unh.edu>

--- src/api/urls.py
+++ src/api/urls.py
@@ -54,6 +54,14 @@ from api.views import (
     extend_booking,
     all_users,
     list_labs
+    resource_ci_metadata,
+    resource_ci_userdata,
+    resource_ci_userdata_directory,
+    all_images,
+    all_opsyss,
+    single_image,
+    single_opsys,
+    create_ci_file,
 )
 
 urlpatterns = [
@@ -81,6 +89,8 @@ urlpatterns = [
 
     path('resource_inventory/availableTemplates', available_templates),
     path('resource_inventory/<int:template_id>/images', images_for_template),
+
+    path('resource_inventory/cloud/create', create_ci_file),
 
     path('users', all_users),
     path('labs', list_labs),
